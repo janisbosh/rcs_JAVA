5fda9f648c5c8752c420050bb34a29e279f34f48
janisbosh <143744458+janisbosh@users.noreply.github.com>
2023-11-09 21:06:20 +0200

    commit

--- Day 2/Day2_if/src/main/java/org/example/Main.java
+++ Day 2/Day2_if/src/main/java/org/example/Main.java
@@ -6,7 +6,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        sample2 ();
+        sample4();
+    }
+    public static void  sample4() {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ievadi skaitli");
+            int a = sc.nextInt();
+            if (a >5){
+                System.out.println("###");
+    }else{  (a < 5) {
+        System.out.println("####");
+    }
+
+    }
+
+         public static void  sample3() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ievadi1 skaitli");
+            int a = sc.nextInt();
+
+            //ievadit "ja",ja skaitlis ir pozitivs un "ne:,ja skaitlis negativs un izvadit kludas pazinojumu ja ir 0
+             //>
+             //<
+             //>=
+             //<=
+             //==
+             //!=
+             if (a > 0) {
+    System.out.println("Ja");
+}else if (a < 0) {
+                 System.out.println("Ne");
+             }else{
+                 System.out.println("Kluda");
+}
+            // String if
+             // ir vai nav vienads
 
     }
     public static void  sample2 () {
