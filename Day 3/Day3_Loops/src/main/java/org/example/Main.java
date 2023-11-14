7d8e1220e802872743179c8e6d0fe715d78b5610
janisbosh <143744458+janisbosh@users.noreply.github.com>
2023-11-14 21:33:17 +0200

    comits2

--- Day 3/Day3_Loops/src/main/java/org/example/Main.java
+++ Day 3/Day3_Loops/src/main/java/org/example/Main.java
@@ -1,9 +1,74 @@
+package org.example;
+
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+        task4();
     }
+
+    public static void task4() {
+        Scanner sc = new Scanner(System.in);
+        String name ="ziema";
+        String input = "";
+
+        while (!input.equals(name)) {
+            System.out.println("ievadi vardu");
+            input = sc.nextLine();
+
+            if (!input.equals(name)) {
+                System.out.println("Megini vel");
+
+            }
+        }
+        System.out.println("Uzmineji");
+}
+
+    public static void task3() {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ievadi skaitli");
+            int number = sc.nextInt();
+
+            if (number > 0) {
+                sum = sum + number;
+            } else {
+                System.out.println("kluda");
+            }
+            System.out.println("kopejais garums ir " + sum);
+        }
+    }
+
+
+    public static void task2() {
+        Scanner sc = new Scanner(System.in);
+
+        int len = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ievadi vertibu");
+            String input = sc.nextLine();
+            len = len + input.length();
+        }
+        System.out.println("kopejais garums ir " + len);
+    }
+
+    public static void sample2() {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Ievadi skaitli");
+            int number = sc.nextInt();
+            sum = sum + number;
+            System.out.println(number);
+            System.out.println("--------------");
+        }
+        System.out.println("Summa ir " + sum);
+    }
+
     public static void task1() {
         Scanner sc = new Scanner(System.in);
 
