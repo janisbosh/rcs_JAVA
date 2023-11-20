032fccdecb9bb267afbad98ba41e0c3eec8a89d7
janisbosh <143744458+janisbosh@users.noreply.github.com>
2023-11-20 20:07:19 +0200

    commits5

--- Day 4/Day4_classes/src/main/java/org/example/Main.java
+++ Day 4/Day4_classes/src/main/java/org/example/Main.java
@@ -1,16 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
-
-        String a = "aaaa";
-        String b = "bbbb";
-        String c = "cccc";
-
-        sample1(a, b, c);
+    task2(a);
+}
+public static void task2 (int a){
+        for (int i = 0; i < a; i++) {
+        for (int j = 0; j <= i; j++) {
+            System.out.print("$");
+        }
+        System.out.println();
     }
+        }
+
 
     public static void sample1(String a, String b, String c) {
         System.out.println(c + b + a);
