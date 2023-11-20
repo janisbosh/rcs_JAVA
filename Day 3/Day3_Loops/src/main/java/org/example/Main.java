88a39f82d8b29972731b4fdb4a372e1842c1fe79
janisbosh <143744458+janisbosh@users.noreply.github.com>
2023-11-20 18:55:54 +0200

    commits1

--- Day 3/Day3_Loops/src/main/java/org/example/Main.java
+++ Day 3/Day3_Loops/src/main/java/org/example/Main.java
@@ -12,7 +12,8 @@ import java.util.Random;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-                md2();
+
+        md2();
     }
 
     public static void md2() {
---
+++ Day 4/Day4_classes/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        String a = "aaaa";
+        String b = "bbbb";
+        String c = "cccc";
+
+        sample1(a,b,c);
+    }
+
+    public static void sample1(String a, String b, String c) {
+        System.out.println(c + a + b);
+    }
+}
