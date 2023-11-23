94a9475d5679c59e353c71926db53ca05279456f
janisbosh <143744458+janisbosh@users.noreply.github.com>
2023-11-23 21:18:56 +0200

    commit

---
+++ Day 5/Day_6/src/main/java/org/example/Samples.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+public class Samples {
+    public static void  sample2 (){
+        int [] a = {3,5,77,9,1};
+        //or (int i=0; i <6; i++){
+            System.out.println(a[i]);
+
+    public static void  sample1 (){
+        int [] a = new int[6];
+        a [0] = 3;
+        a [1] = 3;
+        a [2] = 77;
+        a [3] = 3;
+        a [4] = 9;
+        a [5] = 3;
+
+        for (int i=0; i <6; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
---
+++ Day 5/Day_6/src/main/java/org/example/Tasks.java
@@ -0,0 +1,55 @@
+package org.example;
+import java.util.Scanner;
+public class Tasks {
+
+    String[] stringArray = new String[5];
+    scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < stringArray.length; i++) {
+        System.out.print("Ievadiet vērtību " + (i + 1) + ": ");
+        stringArray[i] = scanner.nextLine();
+    }
+System.out.println("Masīva elementi un to garumi:");
+        for (int i = 0; i < stringArray.length; i++) {
+        System.out.println("Element " + (i + 1) + ", Garums: " + stringArray[i].length());
+    }
+}
+
+    public static void  run () {
+inputNumbers();
+    }
+   private static void  inputNumbers () {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Ievadiet skaitli");
+       int input = scanner.nextInt();
+       int smallest=0;
+
+
+       while (true){
+           smallest =input;
+           System.out.print("Ievadiet skaitli");
+
+       if (input < smallest) {
+           System.out.println("Hellow world");
+       }else{
+           break;
+       }
+       }
+   }
+
+
+
+    public static Double averagenUMBER(int count) {
+        Scanner scanner = new Scanner(System.in);
+
+        double sum = 0.0;
+        for (int i = 0; i < count; i++) {
+            System.out.print("Ievadiet skaitli");
+            double number = scanner.nextInt();
+            sum += number;
+
+        }
+                return sum/count;
+            }
+        }
+
+
