87ebe69506b85c7a23ea730e10aa7bbb06bcca9a
janisbosh <143744458+janisbosh@users.noreply.github.com>
2023-11-28 19:14:19 +0200

    1

---
+++ Day_7/Day_lists/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double[] numbers = { 2.5, 8.9, 5.4, 1.2, 7.3 };
+        System.out.println(Tasks.task1(numbers));
+    }
+}
