5b79be020ef726ed38055fb2ba54b97e687a609c
janisbosh <143744458+janisbosh@users.noreply.github.com>
2023-11-28 21:36:20 +0200

    3

---
+++ Day_7/Day_lists/src/main/java/org/example/Samples.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Samples {
+
+    public static void run () {
+        sample1();
+    }
+    private static void sample1 (){
+        List<Integer> numberList = new ArrayList<>()
+        numberList.add(3);
+        numberList.add(4);
+
+        System.out.println(numberList.get(0));
+        for (int i=0; i<n)
+
+    }
+}
